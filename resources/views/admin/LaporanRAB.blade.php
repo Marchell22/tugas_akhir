85c798ed492f55b4f52fab6e35530092f237b848
Marchell22 <ramotmanurung424@gmail.com>
2024-09-03 14:04:02 +0700

    Rapihin

--- resources/views/admin/LaporanRAB.blade.php
+++ resources/views/admin/LaporanRAB.blade.php
@@ -238,20 +238,7 @@
 
             <section class="store-user mt-5">
                 <div class="col-10">
-                    {{-- <div class="row bb pb-3">
-                        <div class="col-7">
-                            <p>Supplier,</p>
-                            <h2>Your Store Name</h2>
-                            <p class="address"> 777 Brockton Avenue, <br> Abington MA 2351, <br>Vestavia Hills AL </p>
-                            <div class="txn mt-2">TXN: XXXXXXX</div>
-                        </div>
-                        <div class="col-5">
-                            <p>Client,</p>
-                            <h2>Sabur Ali</h2>
-                            <p class="address"> 777 Brockton Avenue, <br> Abington MA 2351, <br>Vestavia Hills AL </p>
-                            <div class="txn mt-2">TXN: XXXXXXX</div>
-                        </div>
-                    </div> --}}
+                    
                     <div class="row extra-info pt-3">
                         <div class="col-7">
                             <p>Bidang: <span> {{ $rencanaAnggaranBiaya->bidang }}</span></p>
@@ -294,75 +281,11 @@
                 </table>
             </section>
 
-            {{-- <section class="balance-info">
-                <div class="row">
-                    <div class="col-8">
-                        <p class="m-0 font-weight-bold"> Note: </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In delectus, adipisci vero est
-                            dolore praesentium.</p>
-                    </div>
-                    <div class="col-4">
-                        <table class="table border-0 table-hover">
-                            <tr>
-                                <td>Sub Total:</td>
-                                <td>800$</td>
-                            </tr>
-                            <tr>
-                                <td>Tax:</td>
-                                <td>15$</td>
-                            </tr>
-                            <tr>
-                                <td>Deliver:</td>
-                                <td>10$</td>
-                            </tr>
-                            <tfoot>
-                                <tr>
-                                    <td>Total:</td>
-                                    <td>825$</td>
-                                </tr>
-                            </tfoot>
-                        </table>
-
-                        <!-- Signature -->
-                        <div class="col-12">
-                            <img src="" class="img-fluid" alt="">
-                            <p class="text-center m-0"> Director Signature </p>
-                        </div>
-                    </div>
-                </div>
-            </section> --}}
-
+            
             <!-- Cart BG -->
             <img src="" class="img-fluid cart-bg" alt="">
 
-            {{-- <footer>
-                <hr>
-                <p class="m-0 text-center">
-                    View THis Invoice Online At - <a href="#!"> invoice/saburbd.com/#868 </a>
-                </p>
-                <div class="social pt-3">
-                    <span class="pr-2">
-                        <i class="fas fa-mobile-alt"></i>
-                        <span>0123456789</span>
-                    </span>
-                    <span class="pr-2">
-                        <i class="fas fa-envelope"></i>
-                        <span>me@saburali.com</span>
-                    </span>
-                    <span class="pr-2">
-                        <i class="fab fa-facebook-f"></i>
-                        <span>/sabur.7264</span>
-                    </span>
-                    <span class="pr-2">
-                        <i class="fab fa-youtube"></i>
-                        <span>/abdussabur</span>
-                    </span>
-                    <span class="pr-2">
-                        <i class="fab fa-github"></i>
-                        <span>/example</span>
-                    </span>
-                </div>
-            </footer> --}}
+          
         </div>
     </div>
 </body>
